7cd7d9aa66c2a17fab05fec223d98d5d680b5536
george <george>
2019-10-20 14:25:26 -0400

    make 2-port matrix like

--- stoz.py
+++ stoz.py
@@ -5,12 +5,11 @@ import skrf as rf
 def main(filename):
     net = rf.Network(filename)
     n = net.z[0][0].size
-    print("# MHZ Z RI")
-    if n == 2: print("!             Z11               Z12               Z21               Z22")
+    print("# MHZ Z RI R 1")
     for f, zij in zip(net.f, net.z):
         print('{:<8g}'.format(f / 1e6), end="")
         for i, x in enumerate(zij.flatten()):
-            if i > 0 and n > 2 and i % n == 0: print("\n        ", end="")
+            if i > 0 and i % n == 0: print("\n        ", end="")
             print(' {:8.4f} {:8.4f}'.format(x.real, x.imag), end="")
         print()
 
