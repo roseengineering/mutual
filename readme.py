5a004aed9a4bc15aa33033e71f42cfe52680e4ac
george <george>
2019-11-02 16:02:53 -0400

    changed tline function with divider1 and divider2

--- readme.py
+++ readme.py
@@ -41,8 +41,8 @@ The program takes the following command line options:
 -segs      : number of segments to divide the simulated vertical into
 -autoload  : automatically base load the element vertical antenna
 -el        : x,y location of a vertical antenna element
--currents  : list of antenna elements currents j or / complex notation
--tlines    : list of transmission lines in j or / complex notation (used with currents)
+-current   : list of antenna elements currents j or / complex notation
+-tline     : list of transmission lines in j or / complex notation (used with currents)
            : ... for divider1 and divider2 lines are between tuning unit and phaser
 -vf        : velocity factor of the transmission line if angle given in feet or meters
 -tie       : list of lines to tie together
@@ -95,23 +95,23 @@ The Orr design above but for multiple frequencies.
 
 The Orr design above but solving with element currents using / complex notation.  (Use a / between the magnitude and the angle in degrees).
 
-{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -currents 1,1/-90") }
+{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -current 1,1/-90") }
 
 ... or using j notation for the complex currents.
 
-{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -currents 1,-j") }
+{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -current 1,-j") }
 
 The Orr design above with the Christman matching transmission lines.
 
-{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -currents 1,-j -tlines 75/90.64,75/176.205") }
+{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -current 1,-j -tline 75/90.64,75/176.205") }
 
 ... or using feet instead of degrees.
 
-{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -currents 1,-j -tlines 75/23.19ft,75/45.10ft") }
+{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -current 1,-j -tline 75/23.19ft,75/45.10ft") }
 
 The Orr desgin above but with 90 degree transmission lines to the elements for current forcing.
 
-{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -currents 1,-j -tlines 75/90,75/90") }
+{ run("mutual -loss 9 -diam .7in -height 33.4ft -freq 7.1 -el 0,0 -el 35.14ft,0 -current 1,-j -tline 75/90,75/90") }
 
 The Orr design above but 20 feet high and "autoloaded".
 
@@ -119,31 +119,31 @@ The Orr design above but 20 feet high and "autoloaded".
 
 All designs from Orr on page 149.
 
-{ run("mutual -loss 9 -diam .7in -freq 3.6  -height 66.8ft -el 0,0 -el 70.28ft,0 -currents 1,-j -tlines 75/46.39ft,75/92.77ft") }
-{ run("mutual -loss 9 -diam .7in -freq 7.1  -height 33.4ft -el 0,0 -el 35.14ft,0 -currents 1,-j -tlines 75/23.19ft,75/45.10ft") }
-{ run("mutual -loss 9 -diam .7in -freq 10.1 -height 23.2ft -el 0,0 -el 24.36ft,0 -currents 1,-j -tlines 75/16.07ft,75/32.15ft") }
-{ run("mutual -loss 9 -diam .7in -freq 14.1 -height 16.7ft -el 0,0 -el 17.57ft,0 -currents 1,-j -tlines 75/11.60ft,75/22.55ft") }
+{ run("mutual -loss 9 -diam .7in -freq 3.6  -height 66.8ft -el 0,0 -el 70.28ft,0 -current 1,-j -tline 75/46.39ft,75/92.77ft") }
+{ run("mutual -loss 9 -diam .7in -freq 7.1  -height 33.4ft -el 0,0 -el 35.14ft,0 -current 1,-j -tline 75/23.19ft,75/45.10ft") }
+{ run("mutual -loss 9 -diam .7in -freq 10.1 -height 23.2ft -el 0,0 -el 24.36ft,0 -current 1,-j -tline 75/16.07ft,75/32.15ft") }
+{ run("mutual -loss 9 -diam .7in -freq 14.1 -height 16.7ft -el 0,0 -el 17.57ft,0 -current 1,-j -tline 75/11.60ft,75/22.55ft") }
 
 Solve the matching network for a 3-element in-line, quarter-wave spacing array using the K2BT method.
 
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -el 130ft,0 -currents 1,1/-90,-1 -k2bt") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -el 130ft,0 -current 1,1/-90,-1 -k2bt") }
 
 Solve the matching network for a 4-square, quarter-wave spacing array using the K2BT method.
 
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 0,65ft -el 65ft,0 -el 65ft,65ft -currents 1,-j,-j,-1 -k2bt -tlines 50/100,50/100,50/100,50/100 -tie 2,3") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 0,65ft -el 65ft,0 -el 65ft,65ft -current 1,-j,-j,-1 -k2bt -tline 50/100,50/100,50/100,50/100 -tie 2,3") }
 
 Solve the matching network for a 2-element, quarter-wave spacing array using the K2BT method.
 
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -currents 1,-j -k2bt") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -current 1,-j -k2bt") }
 
 Solve the matching network for a 2-element, quarter-wave spacing array using the ohms law power divider method.
 
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -currents 1,-j -divider1") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -current 1,-j -divider1") }
 
 Solve the matching network for a 2-element, quarter-wave spacing array using the tee power divider method.
 
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -currents 1,-j -divider2 90") }
-{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -currents 1,-j -divider2 -90") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -current 1,-j -divider2 90") }
+{ run("mutual -diam .7in -height 62.7ft -freq 3.8 -el 0,0 -el 65ft,0 -current 1,-j -divider2 -90") }
 """)
 
 
